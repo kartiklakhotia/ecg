a4cb8d5b6ee74fe8a78e333927d0034532b9bc2e
kartiklakhotia <kartiklakhotia@gmail.com>
2014-02-06 11:29:11 +0530

    cleaning up after multi threading

--- src/hermite.h
+++
@@ -1,6 +0,0 @@
-
-double nFactor(int N);
-double basisScaleFactor(int N, double sigma, double x);
-double hermitePolynomial(int N, double x);
-double hermiteBasisFunction(int N, double sigma, double x);
-void normalizedHermiteBasisFunction(int N, double sigma, double* nhf);
