886bdec4829abc28e603ccd85b8eed77f98c3357
kartiklakhotia <kartiklakhotia@gmail.com>
2014-03-22 18:19:17 +0530

    all filter functions written

--- filter/filter.c
+++ filter/filter.c
@@ -12,13 +12,15 @@ void __loop_pipelining_on__(int A, int B, int C) {}
 void __loop_pipelining_on__(int, int, int);
 #endif
 
-int beatSample;
+//global variables and pointers (could be converted to static if ahir permits and if its better to do so)
 int LPy1, LPy2, LPbuff_ptr;
 int HPy1, HPbuff_ptr;
+int DERIVbuff_ptr;
+int WINsum, WINbuff_ptr;
 
 
-//initialize to a reset state
-void init()
+//initialize all buffers and pointers
+void initfilt()
 {
 	int index;
 	for (index = 0; index < LPbuff_size; index++)
@@ -31,17 +33,25 @@ void init()
 		__loop_pipelining_on__(7,2,0);
 		HPbuff[index]=0;
 	} 
+	for (index = 0; index < DERIVbuff_size; index++)
+	{
+		__loop_pipelining_on__(7,2,0);
+		DERIVbuff[index]=0;
+	} 
 	LPy1 = 0;
 	LPy2 = 0;
 	HPy1 = 0;
+	WINsum = 0;
 	LPbuff_ptr = 0;
 	HPbuff_ptr = 0;
+	DERIVbuff_ptr = 0;
+	WINbuff_ptr = 0;
 }
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 // Digital Low-pass Filter
-// Difference equation is as follows :
+// Difference equation is given by :
 /*
  	y[n] = 2*y[n-1] - y[n-2] + x[n] - 2*x[n-24ms(LPbuff_size/2)] + x[n-48ms(LPbuff_size)]
 */
@@ -52,8 +62,8 @@ void lpfilt()
 		//also, the further Hermite coeffs calculator is working on double precision FP data
 	int output, halfPtr; //halfPtr represents the sample that occurred 24ms ago
 
-	int datum = dataSample; //avoid reading global variables at too many points
-	int ptr = LPbuff_ptr;
+	int datum = read_uint32("sample_pipe");
+	int ptr = LPbuff_ptr; //avoid reading global pointer value at multiple places
 
 	halfPtr = ptr-LPbuff_halfSize;
 	if (halfPtr < 0)
@@ -76,7 +86,7 @@ void lpfilt()
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 // Digital High-pass Filter
-// Difference equation is as follows :
+// Difference equation is given by :
 /*
  	y[n] = y[n-1] + x[n] - x[n-128ms(HPbuff_size)]
 	z[n] = x[n-64ms(HPbuff_halfSize)] - y[n]
@@ -106,3 +116,82 @@ void hpfilt()
 	HPbuff_ptr = ptr;
 	write_uint32("HPout_pipe", output);
 }		
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Digital derivative approximation
+// Difference equations is given by :
+/*
+	y[n] = x[n] - x[n-10ms(DERIVbuff_size)]
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////
+void deriv()
+{
+	int datum = read_uint32("HPout_pipe");
+	int ptr = DERIVbuff_ptr;
+
+	int output = datum - DERIVbuff[ptr];
+	DERIVbuff[ptr] = datum;
+	if (ptr == DERIV_maxptr)
+		ptr = 0;
+	else
+		ptr = ptr + 1;
+	DERIVbuff_ptr = ptr;
+	write_uint32("DERIVout_pipe", output);
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Moving window integrator
+// Returns average of all samples over window width
+/* Sum is calculated as given in following difference equation :
+	sum[n] = sum[n-1] + x[n] - x[n-80ms(WINbuff_size)] 
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////
+void mvwin()
+{
+	int datum = read_uint32("DERIVout_pipe");
+	int ptr = WINbuff_ptr; 
+	int output;
+
+	WINsum = WINsum + datum - WINbuff[ptr];	
+	if (WINsum > WINsum_saturation)
+		output = WINout_saturation;
+	else					// why this saturation value
+		output = sum/WINbuff_size; 	// also, doesn't saturate sum. Sum is stored as it is without clipping but output is clipped
+
+	WINbuff[ptr] = datum;
+	if (ptr == WIN_maxptr)
+		ptr = 0;
+	else
+		ptr = ptr + 1;
+	WINbuff_ptr = ptr;	
+	write_uint32("WINout_pipe", output);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Parent Function
+// Will call all above defined functions in correct sequence
+//////////////////////////////////////////////////////////////////////////////////////////////
+void QRSfilt()
+{
+	initfilt();
+	while(1){
+		int beatSample = read_uint32("input_pipe");
+		write_uint32("sample_pipe", beatSample); //both can be clubbed by reading input pipe directly in the lpfilt()
+	
+		lpfilt();	
+		hpfilt();
+		deriv();
+		mvwin();
+		int output = read_uint32("WINout_pipe");
+		write_uint32("output_pipe", output); // both can be clubbed by writing output pipe directly in mvwin()
+	}
+}
+
+
+
+
--- filter/filter.h
+++ filter/filter.h
@@ -1,24 +1,34 @@
 //200 samples per heartbeat
 //constants for different filter stages
+//check readme for the source of constant values used in the program
+
 #define sample_rate 200
 #define ms_per_sample ((double)1000/(double)sample_rate)
 
-#define LPfilt_range ((int)(25/ms_per_sample + 0.5))
-#define HPfilt_range ((int)(125/ms_per_sample + 0.5))
+//#define LPfilt_range ((int)(25/ms_per_sample + 0.5)) // why 25, buff size should be upto 48ms, hence, should use 24 here ?? 
+						     // however, with ms_per_sample 5, it doesn't make difference
 
-#define LPbuff_size 2*LPfilt_range
-#define HPbuff_size HPfilt_range
-#define window_width ((int)(80/ms_per_sample + 0.5))
+#define LPbuff_size ((int)(48/ms_per_sample + 0.5)) 
+#define HPbuff_size ((int)(125/ms_per_sample + 0.5))
+#define DERIVbuff_size ((int)(10/ms_per_sample + 0.5))
+#define WINbuff_size ((int)(80/ms_per_sample + 0.5))
 
 #define LP_maxptr LPbuff_size-1 				
-#define HP_maxptr HPbuff_size-1 //max permissible indices for LP and HP circular buffers
+#define HP_maxptr HPbuff_size-1 
+#define DERIV_maxptr DERIVbuff_size-1 
+#define WIN_maxptr WINbuff_size-1 //max permissible indices for memory spaces constructing circular buffers
 
 #define LPbuff_halfSize LPbuff_size/2
 #define HPbuff_halfSize HPbuff_size/2
 
-//global memory spaces (can be replaced with static if it works with ahir)
+#define WINout_saturation 32000
+#define WINsum_saturation WINout_saturation*WINbuff_size
+
+//global memory spaces
 
 int LPbuff[LPbuff_size];
 int HPbuff[HPbuff_size];
+int DERIVbuff[DERIVbuff_size];
+int WINbuff[WINbuff_size];
 
-
+void QRSfilt();
