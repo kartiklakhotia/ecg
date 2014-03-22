95f0527f6b1f099343ff0e609bd6261a6bcd6a46
kartiklakhotia <kartiklakhotia@gmail.com>
2014-03-22 16:38:58 +0530

    added lp and hp filter functions

--- filter/filter.c
+++ filter/filter.c
@@ -14,7 +14,7 @@ void __loop_pipelining_on__(int, int, int);
 
 int beatSample;
 int LPy1, LPy2, LPbuff_ptr;
-int HPy1, HPy2, HPbuff_ptr;
+int HPy1, HPbuff_ptr;
 
 
 //initialize to a reset state
@@ -34,42 +34,75 @@ void init()
 	LPy1 = 0;
 	LPy2 = 0;
 	HPy1 = 0;
-	HPy2 = 0;
 	LPbuff_ptr = 0;
 	HPbuff_ptr = 0;
 }
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////
 // Digital Low-pass Filter
 // Difference equation is as follows :
 /*
- 	y[n] = 2*y[n-1] - y[n-2] + x[n] - 2*x[t-24ms(LPbuff_size/2)] + x[t-48ms(LPbuff_size)]
+ 	y[n] = 2*y[n-1] - y[n-2] + x[n] - 2*x[n-24ms(LPbuff_size/2)] + x[n-48ms(LPbuff_size)]
 */
+/////////////////////////////////////////////////////////////////////////////////////////////
 void lpfilt()
 {
-	long y0; //check if long is required, what about normalization
+	long LPy0; //check if long is required, what about normalization
 		//also, the further Hermite coeffs calculator is working on double precision FP data
 	int output, halfPtr; //halfPtr represents the sample that occurred 24ms ago
-	int datum = dataSample; //avoid reading global variable at too many places
+
+	int datum = dataSample; //avoid reading global variables at too many points
+	int ptr = LPbuff_ptr;
+
 	halfPtr = ptr-LPbuff_halfSize;
 	if (halfPtr < 0)
 		halfPtr += LPbuff_size;
-	y0 = (LPy1 << 1) - LPy2 + datum - (LPbuff[halfPtr] << 1) + LPbuff[ptr];		
+	LPy0 = (LPy1 << 1) - LPy2 + datum - (LPbuff[halfPtr] << 1) + LPbuff[ptr];		
 	LPy2 = LPy1;
-	LPy1 = y0;
-	output = y0 / (LPbuff_halfSize*LPbuff_halfSize); // what is the reason for this 
+	LPy1 = LPy0;
+	output = LPy0 / (LPbuff_halfSize*LPbuff_halfSize); // what is the reason for this 
 							// particular scaling coefficient
 	LPbuff[ptr] = datum;
 	if (ptr == LP_maxptr)
 		ptr = 0;
 	else
 		ptr = ptr + 1;
-		
+	LPbuff_ptr = ptr;		
 	write_uint32("LPout_pipe", output); // send this output for high pass filtering	
 					   // no need to pass argument to and from main function
 }
 
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Digital High-pass Filter
+// Difference equation is as follows :
+/*
+ 	y[n] = y[n-1] + x[n] - x[n-128ms(HPbuff_size)]
+	z[n] = x[n-64ms(HPbuff_halfSize)] - y[n]
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////
+void hpfilt()
+{
+	long HPy0;
+	int output, halfPtr;
 	
-	 
+	int datum = read_uint32("LPout_pipe");
+	int ptr = HPbuff_ptr;
+
+	halfPtr = ptr-HPbuff_halfSize;
+	if (halfPtr < 0)
+		halfPtr += HPbuff_size;
+
+	HPy0 = HPy1 + datum - HPbuff[ptr];
+	HPy1 = HPy0;
+	output = HPbuff[halfPtr] - HPy0;	
+
+	HPbuff[ptr] = datum;
+	if (ptr == HP_maxptr)
+		ptr = 0;
+	else
+		ptr = ptr + 1;
+	HPbuff_ptr = ptr;
+	write_uint32("HPout_pipe", output);
+}		
--- filter/filter.h
+++ filter/filter.h
@@ -10,10 +10,11 @@
 #define HPbuff_size HPfilt_range
 #define window_width ((int)(80/ms_per_sample + 0.5))
 
-#define LP_maxptr LPbuff_size-1 //avoid calculating maximum allowable pointer values in each iteration
-				//keep them predefined
-#define HP_maxptr HPbuff_size-1
+#define LP_maxptr LPbuff_size-1 				
+#define HP_maxptr HPbuff_size-1 //max permissible indices for LP and HP circular buffers
+
 #define LPbuff_halfSize LPbuff_size/2
+#define HPbuff_halfSize HPbuff_size/2
 
 //global memory spaces (can be replaced with static if it works with ahir)
 
